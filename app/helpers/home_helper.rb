1bf16a65c94cbdb0f4e0385dd2f16ff3778118e5
yurich <yurich.maevskiy@gmail.com>
2017-03-27 17:28:32 +0300

    autoschool baner

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,17 @@
 module HomeHelper
+
+  def stars_generator(count = 0)
+    tmp = ''
+    5.times do |i|
+      tmp.concat(made_stars(count - 1 >= i ))
+    end
+    tmp
+  end
+
+  private
+
+  def made_stars(full)
+    full ? '<i class="ion-ios-star"></i>' : '<i class="ion-ios-star empty-star"></i>'
+  end
+
 end
