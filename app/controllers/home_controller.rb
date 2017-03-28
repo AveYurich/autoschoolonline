02f15e9fb968972bf543f6ebd81a8a90b0309977
yurich <yurich.maevskiy@gmail.com>
2017-03-28 17:00:09 +0300

    autoschool sidebar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,6 @@ class HomeController < ApplicationController
   end
 
   def show
+    render 'autoschool/index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
     root 'home#index'
-    get 'show', to: 'home#show'
+    get 'autoschool', to: 'home#show'
   end
 
 end
